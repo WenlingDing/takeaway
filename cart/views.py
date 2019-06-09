8f24dfbbd45c291a15c699e721758028613a8758
wenling <dwlwendy18@gmail.com>
2019-06-09 09:01:40 +0000

    clean up

--- cart/views.py
+++ cart/views.py
@@ -10,11 +10,16 @@ def view_cart(request):
 
 @login_required
 def add_to_cart(request, id):
+    """Add a quantity of the specified product to the cart"""
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
-    cart[id] = cart.get(id, quantity)
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity      
+    else:
+        cart[id] = cart.get(id, quantity) 
+
     request.session['cart'] = cart
-    return redirect(index)
+    return redirect('index')
 
     
 @login_required
